41b49e6a66f0cbe07e38ec5c4219e3a7eacd988c
Peter Cai <peter@typeblog.net>
2019-06-26 16:10:21 +0200

    grus: Enable and configure FOD HAL
    Change-Id: I0c034def5803ab80e6158a4c4d1caf1eeaa92841
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- device.mk
+++ device.mk
@@ -41,6 +41,13 @@ $(call inherit-product, device/xiaomi/sdm710-common/sdm710.mk)
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_sdm710
+
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/etc/manifest.xml
