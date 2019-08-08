42771ca04cff3b9dc101a65c5aba61e493bfdc99
Albert I <kras@raphielgang.org>
2019-08-08 22:51:33 +0800

    grus: Import device-specific stock media profiles configuration
    This has to be decommonized as grus lacks support for highspeed profiles.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/etc/manifest.xml
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
