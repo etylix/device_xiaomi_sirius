22d1ecb1c81ff32679f9b720cf73667f83a98d4e
etylix <hkkadpro@gmail.com>
2019-09-15 23:24:42 +0700

    Revert "grus: Align NFC configs with upstream"
    This reverts commit 11eafdc30b0b77fcfddf5e42c6481595f4e8d994.

--- device.mk
+++ device.mk
@@ -58,7 +58,3 @@ PRODUCT_PACKAGES += \
     NQNfcNci \
     SecureElement \
     Tag
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
